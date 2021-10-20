a4762f92122018a0032240cbef81a7c5a0a342e4
Dorian Buck <dorianbuck@gmail.com>
2021-10-20 13:01:30 +0200

    adds visibilty toggle to cart in menu

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { List, Image } from "semantic-ui-react";
+import { useSelector } from "react-redux"
 
 const MenuItem = ({ item, addToCart }) => {
+  const { authenticated } = useSelector((state) => state);
+
   return (
     <List.Item data-cy={`item-${item.id}`}>
       <Image data-cy="image" src={item.image} size="mini" floated="right" />
@@ -12,13 +15,17 @@ const MenuItem = ({ item, addToCart }) => {
         </List.Description>
         <List.Item data-cy="price">{item.price} kr</List.Item>
       </List.Content>
-      <List.Icon
-        onClick={() => addToCart(item.id)}
-        floated="right"
-        name="cart"
-        link
-        data-cy={`add-to-basket-${item.id}`}
-      ></List.Icon>
+      {!authenticated ? (
+        <div></div>
+      ) : (
+        <List.Icon
+          onClick={() => addToCart(item.id)}
+          floated="right"
+          name="cart"
+          link
+          data-cy={`add-to-basket-${item.id}`}
+        ></List.Icon>
+      )}
     </List.Item>
   );
 };
